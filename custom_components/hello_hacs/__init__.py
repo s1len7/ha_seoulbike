ae4f69c3cd9b7d0c745cd50a70f34e3fef672d19
silent <hyeongil.ko@gmail.com>
2026-06-25 12:33:48 +0900

    Add logging to async_setup for Hello HACS integration

--- custom_components/hello_hacs/__init__.py
+++ custom_components/hello_hacs/__init__.py
@@ -1,6 +1,10 @@
 """Hello HACS integration."""
+import logging
 
 DOMAIN = "hello_hacs"
+_LOGGER = logging.getLogger(__name__)
 
 async def async_setup(hass, config):
+    # Home Assistant 시작 시 1회 실행
+    _LOGGER.info("Hello World from hello_hacs!")
     return True
